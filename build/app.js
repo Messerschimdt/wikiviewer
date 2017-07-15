dd822e2f8aef2af3cf33e7427a196e77f7712d24
codekendo <brandonwong@fastmail.com>
2017-07-15 16:01:10 -0700

    changed the way I call the api with new headers

--- build/app.js
+++ build/app.js
@@ -1,34 +1,51 @@
 var wiki = angular.module('wiki', []);
 
-
-wiki.controller('wikiView',function ($scope, $http){
-  $scope.textInput='';
-  $scope.$watch('textInput',function(){
-    fetch();})
-  function fetch (){
-  console.log('click');
-      $http.get('https://en.wikipedia.org/w/api.php?action=opensearch&search=' + $scope.textInput + '&format=json')
-      .then(
-        function successCallback(response) {
+wiki.controller('wikiView', function($scope, $http) {
+  $scope.textInput = '';
+  $scope.$watch('textInput', function() {
+    fetch();
+  })
+  function fetch() {
+    $.ajax({
+      type: "GET",
+      url: 'https://en.wikipedia.org/w/api.php?action=opensearch&search=' + $scope.textInput + '&format=json',
+      dataType: "json",
+      headers: "ORIGN" success: function(response) {
         $scope.wikidata = response.data;
 
+        $scope.filterData = [];
 
-        $scope.filterData=[];
-
-
-        for (var i =0; i <$scope.wikidata[1].length ; i++){
-        $scope.filterData.push(
-          {
-          title :$scope.wikidata[1][i],
-           desc : $scope.wikidata[2][i],
-            link:$scope.wikidata[3][i]
-          }
-                   );
+        for (var i = 0; i < $scope.wikidata[1].length; i++) {
+          $scope.filterData.push({title: $scope.wikidata[1][i], desc: $scope.wikidata[2][i], link: $scope.wikidata[3][i]
+          });
         }
+      }
 
+    })
 
-
-      })//endofthen
-  }//end of fetch
-  $scope.clickSearch = fetch ();
+    //
+    // $http.get()
+    // .then(
+    //   function successCallback(response) {
+    //   $scope.wikidata = response.data;
+    //
+    //
+    //   $scope.filterData=[];
+    //
+    //
+    //   for (var i =0; i <$scope.wikidata[1].length ; i++){
+    //   $scope.filterData.push(
+    //     {
+    //     title :$scope.wikidata[1][i],
+    //      desc : $scope.wikidata[2][i],
+    //       link:$scope.wikidata[3][i]
+    //     }
+    //              );
+    //   }
+    //
+    //
+    //
+    // })//endofthen
+  } //end of fetch
+  $scope.clickSearch = fetch();
 })
